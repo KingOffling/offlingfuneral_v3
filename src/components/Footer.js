5be1e08cfb04c26b1f8c25d0f541ce79aaafe334
KingOffling <125208496+KingOffling@users.noreply.github.com>
2023-04-20 19:46:53 -0400

    ver

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
     <Box bg="#fb5151" p={4} mt={4}>
       <Flex justify="center" alignItems="center">
-
+        <Text>version 2.11</Text>
       </Flex>
     </Box>
   );
